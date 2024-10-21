89d7ff196b6406f3bfccf9fae75caff89fd29caa
Vengadesh27 <m.vengadesh2019@gmail.com>
2024-10-22 00:14:37 +0530

    ui fixes and new pages designed

--- app.py
+++ app.py
@@ -6,5 +6,16 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')    
 
+@app.route('/privacy')
+def privacy():
+    return render_template("privacy.html")
+
+
+@app.route('/terms')
+def terms():
+    return render_template("terms.html")
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
